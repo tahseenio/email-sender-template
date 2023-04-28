120bdd5ee62d2b8cadea8f81b8167cf2e161340d
Tahseen <tahseenislam@hotmail.com>
2023-04-28 19:19:12 +1000

    added cron job

---
+++ dist/config.js
@@ -0,0 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+export const SENDGRID_APIKEY = process.env.SENDGRID_APIKEY;
+export const FROM_EMAIL = process.env.FROM_EMAIL;
+export const TO_EMAIL = process.env.TO_EMAIL;
+//# sourceMappingURL=config.js.map
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { CronJob } from 'cron';
 import sgMail from '@sendgrid/mail';
 import { SENDGRID_APIKEY, FROM_EMAIL, TO_EMAIL } from './config.js';
 sgMail.setApiKey(SENDGRID_APIKEY);
@@ -13,20 +14,33 @@ interface msgProps {
 const msg: msgProps = {
   to: [`${TO_EMAIL}`],
   from: `${FROM_EMAIL}`,
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
+  subject: 'Sending with SendGrid at 8pm',
+  text: 'This is your 8pm email from your node/sendgrid/cron project',
   html: '<strong>HI TSNODE. and easy to do anywhere, even with Node.js. Package: module, From email and to email set in .env</strong>',
 };
 
-sgMail
-  .send(msg)
-  .then((response) => {
-    console.log(response[0].statusCode);
-    // console.log(response[0].headers);
-    console.log('SENT');
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+const sendEmail = (): void => {
+  sgMail
+    .send(msg)
+    .then((response) => {
+      console.log(response[0].statusCode);
+      // console.log(response[0].headers);
+      console.log('SENT');
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
 
-console.log('Ran');
+new CronJob(
+  '* * * * * *',
+  () => {
+    console.log('You will see this message every second');
+    // sendEmail();
+  },
+  null,
+  true,
+  'Australia/Brisbane'
+);
+
+// '0 20 * * *',
